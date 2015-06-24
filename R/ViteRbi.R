950e18d09370380518f4a74d4e04a11b3c92eb03
Tomas Fitzgerald <tf2@sanger.ac.uk>
2015-06-24 09:33:14 +0100

    Update ViteRbi.R

--- R/ViteRbi.R
+++ R/ViteRbi.R
@@ -28,3 +28,29 @@ ViteRbi <- function(data=NULL, states=c(-1,0,1), normalstate=1, emiss = c(-1, 1,
 	
 	invisible(jumpy)
 }
+
+extract_calls <- function(jumpy) {
+	u = unique(jumpy[,4])
+	cnv_calls = NULL
+	start_index = 1
+	start_chr = jumpy[start_index,1]
+	start_start = jumpy[start_index,2]
+	start_state = jumpy[start_index,4]
+	for(x in 2:length(jumpy[,1])) {
+		if(jumpy[x,4]!=start_state) {
+			call = data.frame(start_chr, start_start, jumpy[x-1,2], start_state, mean(jumpy[start_index:(x-1), 3]))
+			colnames(call) = c("chr", "start", "stop", "state", "mean_lr2")
+			cnv_calls = rbind(cnv_calls, call)
+			start_chr = jumpy[x,1]
+			start_start = jumpy[x,2]
+			start_state = jumpy[x,4]
+			start_index = x
+		}
+		if(x==length(jumpy[,1])) {
+			call = data.frame(start_chr, start_start, jumpy[x,2], start_state, mean(jumpy[start_index:x, 3]))
+			colnames(call) = c("chr", "start", "stop", "state", "mean_lr2")
+			cnv_calls = rbind(cnv_calls, call)
+		}
+	}
+return(cnv_calls)
+}
