e4954da29a8fbcb56f8da02eb76d773df7e49e25
Tomas Fitzgerald <tf2@sanger.ac.uk>
2015-06-24 15:24:08 +0100

    Update ViteRbi.R

--- R/ViteRbi.R
+++ R/ViteRbi.R
@@ -1,13 +1,12 @@
 ViteRbi <- function(data=NULL, states=c(-1,0,1), normalstate=1, emiss = c(-1, 1, 0, 1, 1, 1), trans=c(0.99, 0.01, 0, 0.005, 0.99, 0.005, 0, 0.01, 0.99), ep=2, tp=3, active=T) {
 	if(is.null(data)) { data(test); data = test; }
 	jumpy = NULL; normalstates = vector();
-	for(x in 1:length(data[,1])) { normalstates[x] = normalstate; }
 	u = unique(data[,1])
 	for(x in 1:length(u)) {
 		d = data[data[,1]==u[x],]	
 		res <- .C("ViteRbi"
 				,"data" = as.double(d[,3])
-				,"states" = as.double(normalstates)
+				,"states" = as.double(rep(normalstate, length(d[,1])))
 				,"emissions" = as.double(emiss)
 				,"transitions" = as.double(trans)
 				,"dN" = as.integer(length(d[,3]))
@@ -38,8 +37,8 @@ extract_calls <- function(jumpy) {
 	start_state = jumpy[start_index,4]
 	for(x in 2:length(jumpy[,1])) {
 		if(jumpy[x,4]!=start_state) {
-			call = data.frame(start_chr, start_start, jumpy[x-1,2], start_state, mean(jumpy[start_index:(x-1), 3]))
-			colnames(call) = c("chr", "start", "stop", "state", "mean_lr2")
+			call = data.frame(start_chr, start_start, jumpy[x-1,2], start_state, mean(jumpy[start_index:(x-1), 3]), length(jumpy[start_index:(x-1), 3]))
+			colnames(call) = c("chr", "start", "stop", "state", "mean_lr2", "number_probes")
 			cnv_calls = rbind(cnv_calls, call)
 			start_chr = jumpy[x,1]
 			start_start = jumpy[x,2]
@@ -47,8 +46,8 @@ extract_calls <- function(jumpy) {
 			start_index = x
 		}
 		if(x==length(jumpy[,1])) {
-			call = data.frame(start_chr, start_start, jumpy[x,2], start_state, mean(jumpy[start_index:x, 3]))
-			colnames(call) = c("chr", "start", "stop", "state", "mean_lr2")
+			call = data.frame(start_chr, start_start, jumpy[x,2], start_state, mean(jumpy[start_index:x, 3]), length(jumpy[start_index:x, 3]))
+			colnames(call) = c("chr", "start", "stop", "state", "mean_lr2", "number_probes")
 			cnv_calls = rbind(cnv_calls, call)
 		}
 	}
