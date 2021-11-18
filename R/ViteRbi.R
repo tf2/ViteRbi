2d2114f61d527559f187d73be43d987d844c5db8
Ian Brettell <brettell@ebi.ac.uk>
2021-11-18 10:45:25 +0000

    Update ViteRbi.R

--- R/ViteRbi.R
+++ R/ViteRbi.R
@@ -28,6 +28,37 @@ ViteRbi <- function(data=NULL, states=c(-1,0,1), normalstate=1, emiss = c(-1, 1,
 	invisible(jumpy)
 }
 
+ViteRbi_fixed <- function(data=NULL, states=c(-1,0,1), normalstate=1, emiss = c(-1, 1, 0, 1, 1, 1), trans=c(0.99, 0.01, 0, 0.005, 0.99, 0.005, 0, 0.01, 0.99), ep=2, tp=3, active=T) {
+	if(is.null(data)) { data(test); data = test; }
+	jumpy = NULL; normalstates = vector();
+	u = unique(data[,1])
+	for(x in 1:length(u)) {
+		d = data[data[,1]==u[x],]	
+		res <- .C("ViteRbi_fixed"
+				,"data" = as.double(d[,3])
+				,"states" = as.double(rep(normalstate, length(d[,1])))
+				,"emissions" = as.double(emiss)
+				,"transitions" = as.double(trans)
+				,"dN" = as.integer(length(d[,3]))
+				,"sN" = as.integer(length(states))
+				,"eN" = as.integer(ep)
+				,"tN" = as.integer(tp)
+				,"PACKAGE" = "ViteRbi")
+		jumpy = rbind(jumpy, cbind(d[,1:3], res$states))
+		
+		if(active) {
+			par(mfrow=c(2,1))
+			plot(d[,3], xlab="Index", ylab="Value", main="Data")
+			plot(res$states, xlab="Index", ylab="State", main="Estimated States")
+			print("hit return to continue")
+			scan("")
+		}
+	}
+	
+	invisible(jumpy)
+}
+
+
 extract_calls <- function(data) {
 	all_cnv_calls = NULL
 	u = unique(data[,1])
