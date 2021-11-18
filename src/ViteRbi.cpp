791b8fb0b2fa42ebd7049b31789fa818acc4a8d1
Ian Brettell <brettell@ebi.ac.uk>
2021-11-18 10:41:42 +0000

    Update ViteRbi.cpp

--- src/ViteRbi.cpp
+++ src/ViteRbi.cpp
@@ -76,6 +76,32 @@ vector<double> viterbi(hmm h, vector<double> d, vector<double> s) {
         for(int i= d.size()-2;i>=0;i--){ s[i]=bwds[ns][i+1]; ns=s[i]; }
 return s;        
 }
+	
+vector<double> viterbi_fixed(hmm h, vector<double> d, vector<double> s, double *ep; double *tp ) {        
+	int i, j; int N = (int)h.d_nsts; 
+	int **fwds = new int*[N]; int **bwds = new int*[N]; 
+	//vector < double** > rrr = ets(h); double **ep = rrr[0]; double **tp = rrr[1];
+    for(i=0;i<N;++i) { fwds[i] = new int[d.size()]; bwds[i] = new int[d.size()]; }
+    for(i=0;i<N;i++) { fwds[i][0] = log(npdf(ep[i][0], ep[i][1], d[0])); }  
+        for (int i=1;i<d.size();i++) {
+            for (int j=0;j<N;j++) {
+                int pps; double mp; double sps[N];
+                for (int ps = 0; ps < N; ps++) { sps[ps] = fwds[ps][i-1] + log(tp[ps][j]); }
+                mp = sps[0];  pps = 0;
+				for (int k=0;k<N;k++) {
+					if (sps[k] > mp) { mp = sps[k]; pps = k; }
+				}
+                fwds[j][i] = mp + log(npdf(ep[j][0], ep[j][1], d[i])); bwds[j][i] = pps;
+         	}
+        }  
+        int fs=0; double ml=fwds[0][d.size()-1];
+        if(fwds[1][d.size()-1]>ml){ ml=fwds[1][d.size()-1]; fs=1; }
+        if(fwds[2][d.size()-1]>ml){ ml=fwds[2][d.size()-1]; fs=2; }
+        s[d.size()-1]=fs; int ns=fs;
+        for(int i= d.size()-2;i>=0;i--){ s[i]=bwds[ns][i+1]; ns=s[i]; }
+return s;        
+}
+	
 
 vector<double> nord(double *data, int N) {
 	double su=0; int count=0; vector < double > e;
@@ -97,5 +123,14 @@ void ViteRbi(double *data, double *states, double *emissions, double *transition
 	}
 }
 
+void ViteRbi_fixed(double *data, double *states, double *emissions, double *transitions, int *dN, int *sN, int *eN, int *tN, double *ep; double *tp) {
+	vector < double > d = nord(data, (int)*dN);
+	vector < double > s; for(int i=0;i<(int)*dN;i++) { s.push_back(states[i]); }
+	hmm h((int)*sN, (int)*eN, (int)*tN, emissions, transitions);
+	vector< double > r = viterbi_fixed(h, d, s, ep, tp);
+	for(int i=0;i<(int)*dN;i++) {
+		states[i]=r[i];
+	}
+}
 
 }
